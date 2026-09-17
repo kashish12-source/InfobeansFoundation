81b5e471062aad291bfac7912d3acc63f8b375cd
kashish vishnoi <kashishvishnoi2006@gmail.com>
2026-09-17 13:58:52 +0530

    practining loops

---
+++ LOOP/while/question5.py
@@ -0,0 +1,70 @@
+# n=5
+# i=1
+# sum = 0
+# while(i<=n):
+#     sum+=i
+# #     i+=1
+# # print(sum)
+# n = 5
+# result = 1
+# while(n>=2):
+#     result*=n
+#     n-=1
+# print(result)
+
+# n =6
+# i =2 
+# f = False
+# while(i<=n//2):
+#     if(n%i==0):
+#         f= True
+#     i+=1
+# if(f):
+#     print("Not a prime")
+# else:
+#     print("a prime")
+
+
+# fibonacci
+# l=0
+# i =0 
+# j = 1
+# print(i)
+# print(j)
+# while(l<=n-3):
+#     k=i+j
+#     print(k)
+#     i=j
+#     j=k
+#     l+=1
+
+# j=0
+# i=1
+# n=6
+# while(n!=0):
+#    k=i+j
+#    print(k)
+#    i=k
+#    j=j+1
+#    n=n-1
+
+n =8
+# i=1
+# while(n!=1):
+#     if(i==1):
+#         print(f"{i} + ",end="")
+#     else:
+#         print(f"1/{i} + ",end=" ")
+#     i +=1
+#     n-=1
+# print(f"1/{i}")
+# i=0
+# while(n!=0):
+#     print(i*7,end=" ")
+#     i+=1
+#     n-=1
+i=1
+while(n!=0):
+    print(f"{i**3}",end=" ")
+    i+=2
+    n-=1
