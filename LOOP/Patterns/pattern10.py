e966711dd9f8a93a8d148c0550598af00921b1c2
kashish vishnoi <kashishvishnoi2006@gmail.com>
2026-09-20 21:49:10 +0530

    feat: add pattern10 and pattern11 python scripts

---
+++ LOOP/Patterns/pattern10.py
@@ -0,0 +1,8 @@
+k=65
+for i in range(1,5):
+    
+    for j in range(i):
+        print(chr(k),end ="")
+    k+=1
+    print()
+    
---
+++ LOOP/Patterns/pattern11.py
@@ -0,0 +1,8 @@
+k=65+32
+for i in range(1,5):
+    
+    for j in range(i):
+        print(chr(k),end="")
+        k+=1
+   
+    print()
