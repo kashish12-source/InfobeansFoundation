6a86f157ca6852791d82d08bb1a1804fdd7a4bfb
kashish vishnoi <kashishvishnoi2006@gmail.com>
2026-09-20 21:22:52 +0530

    feat: add pattern1.py to print diagonal star pattern

---
+++ LOOP/Patterns/pattern1.py
@@ -0,0 +1,13 @@
+# *
+#  *
+#   *
+#    *
+#     *
+
+for i in range(6):
+    for j in range(6):
+        if(i==j):
+            print("*", end=" ")
+        else:
+            print(" ",end=" ")
+    print()
