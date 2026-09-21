f3e2d514be5333e22b52b92444be70d431d19165
kashish vishnoi <kashishvishnoi2006@gmail.com>
2026-09-21 14:08:54 +0530

    refactor: update loop pattern implementations

--- LOOP/Patterns/pattern22.py
+++ LOOP/Patterns/pattern22.py
@@ -1,7 +1,13 @@
 k=1
-for i in range(1,6):
+for i in range(1,5):
     
     for j in range(0,k):
         print("*",end=" ")
     k+=i
     print()
+
+# * 
+# * * 
+# * * * * 
+# * * * * * * * 
+# * * * * * * * * * * * 
---
+++ LOOP/Patterns/pattern41.py
@@ -0,0 +1,7 @@
+k =65
+for i in range (5):
+    
+    for j in range(2*i-1):
+        print(chr(k),end="")
+        k+=1
+    print()
